359317d1be2842292a808f677a9c4ab3d5557949
Sergiy <sergey.morenets@gmail.com>
2022-09-10 17:25:47 +0300

    Task 7

--- security-task1/src/main/java/it/discovery/security/config/WebSecurityConfiguration.java
+++ security-task1/src/main/java/it/discovery/security/config/WebSecurityConfiguration.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,6 +26,19 @@ public class WebSecurityConfiguration {
 
         http.csrf().disable();
         return http.build();
+    }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .passwordEncoder(passwordEncoder::encode)
+                .username("admin").password("root")
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(admin);
     }
 }
